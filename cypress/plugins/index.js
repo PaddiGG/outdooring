8b4bad868a7c45e8a08bc659fa37d3c76692d1e7
Patrick Wendlandt <p.wendlandt@gmx.de>
2022-05-24 19:29:01 +0200

    initial commit

---
+++ cypress/plugins/index.js
@@ -0,0 +1,13 @@
+/// <reference types="cypress" />
+
+const injectDevServer = require('@cypress/react/plugins/next')
+
+/**
+ * @type {Cypress.PluginConfig}
+ */
+// eslint-disable-next-line no-unused-vars
+module.exports = (on, config) => {
+  injectDevServer(on, config)
+
+  return config
+}
